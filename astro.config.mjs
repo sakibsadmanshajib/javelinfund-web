c45a7ac0d3e78316e0ebe3a2185fc00526c69bce
Sakib Sadman Shajib <mail@sakibsadmanshajib.com>
2026-05-14 15:45:58 -0400

    chore: scaffold Astro project (minimal template + tooling)

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+
+export default defineConfig({
+  site: 'https://javelinfund.ca',
+  output: 'static',
+  trailingSlash: 'never',
+  build: { format: 'directory' },
+  integrations: [sitemap()],
+  image: { service: { entrypoint: 'astro/assets/services/sharp' } },
+});
