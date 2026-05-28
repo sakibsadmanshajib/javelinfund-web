fe7d61fcbb940bc85acc9f848a0c4c4bbeb61a1d
Sakib Sadman Shajib <mail@sakibsadmanshajib.com>
2026-05-28 18:21:39 -0400

    feat(worker): serial formatting helper

---
+++ decap-oauth-worker/src/lib/serial.js
@@ -0,0 +1,10 @@
+export function formatSerial(year, num) {
+  if (num < 1 || num > 9999) throw new Error('serial number out of range (1..9999)');
+  return `${year}-${String(num).padStart(4, '0')}`;
+}
+
+export function parseSerial(serial) {
+  const m = /^(\d{4})-(\d{4})$/.exec(serial);
+  if (!m) throw new Error('bad serial format');
+  return { year: Number(m[1]), num: Number(m[2]) };
+}
